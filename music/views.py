762418e372f3e96d98a965444f4db940d40527ee
rajyavardhan01 <rajya.vardhan01@gmail.com>
2017-09-11 20:39:26 -0400

    template + HttpResponse = render template

--- music/views.py
+++ music/views.py
@@ -9,20 +9,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album # for connecting to models.py database schema
 
+from django.template import loader # for importing templates
+
 def index(request):
 
     # Taking all the album objects from the db using class1.object.all()
     # and displaying it to the home page
     all_albums = Album.objects.all()
 
+    template = loader.get_template('music/index.html') # loader will automatically checks the template folder
+
+    context = {'all_albums': all_albums} # NOTE: context is a dictionary of Album objects
+
+    return HttpResponse(template.render(context, request))
+
+
+"""
     # Creating hyperlinks for each Album object using its id ie. for each Album
     # and then displaying them on the page
     html=''
     for album in all_albums:
         url = '/music/' + str(album.id) + '/'
         html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+"""
 
-    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse('<h2> Details for album id ' + str(album_id) + '</h2>')
