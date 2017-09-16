8bd9749858ffad3a6d54718178055ba54be58f76
rajyavardhan01 <rajya.vardhan01@gmail.com>
2017-09-16 18:28:42 -0400

    Favorite functionality implemented

--- music/urls.py
+++ music/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import url
 
 from . import views # It means look into the same directory i.e. music and import views.py
 
-app = 'music'
+app_name = 'music'
 
 urlpatterns = [
 
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/71
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
+    # /music/71<album_id>
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/71<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from .models import Album # for connecting to models.py database schema
+from .models import Album, Song # for connecting to models.py database schema
 
 from django.shortcuts import render, get_object_or_404
 
@@ -17,3 +17,17 @@ def detail(request, album_id):
 
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song']) # pk=request.POST['song'] is taken from detail.html
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song'
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
